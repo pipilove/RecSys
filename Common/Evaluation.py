ec45b29ef7a8d4b5e59c53b18dcd6a90a5e484ca
pipilove <pipijob@126.com>
2016-07-08 10:09:47 +0800

    add rec evaluation

---
+++ Common/Evaluation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = '推荐系统的评估'
+__author__ = 'pika'
+__mtime__ = '16-7-5'
+__email__ = 'pipisorry@126.com'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓   ┏┓
+            ┏━┛┻━━━┛┻━━┓
+            ┃     ☃    ┃
+            ┃  ┳┛   ┗┳ ┃
+            ┃     ┻    ┃
+            ┗━┓      ┏━┛
+              ┃      ┗━━━┓
+              ┃  神兽保佑 ┣┓
+              ┃　永无BUG！┏┛
+              ┗┓┓┏━━━┳┓┏┛
+               ┃┫┫   ┃┫┫
+               ┗┻┛   ┗┻┛
+"""
+
+
